b82b699c793d7d806b7cf3f0272a0dde3038ebb6
LucasIkeda663 <93748272+LucasIkeda663@users.noreply.github.com>
2021-11-10 09:42:47 -0300

    Create ingrediente.php

---
+++ ingrediente.php
@@ -0,0 +1,157 @@
+<?php
+
+// Verificar se foi enviando dados via POST
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $id = (isset($_POST["id"]) && $_POST["id"] != null) ? $_POST["id"] : "";
+    $nome = (isset($_POST["nome"]) && $_POST["nome"] != null) ? $_POST["nome"] : "";
+    $descricao = (isset($_POST["descricao"]) && $_POST["descricao"] != null) ? $_POST["descricao"] : "";
+
+} else if (!isset($id)) {
+    // Se não se não foi setado nenhum valor para variável $id
+    $id = (isset($_GET["id"]) && $_GET["id"] != null) ? $_GET["id"] : "";
+    $nome = NULL;
+    $descricao = NULL;
+}
+ 
+// Cria a conexão com o banco de dados
+try {
+    $conexao = new PDO("mysql:host=127.0.0.1:3307 ; dbname=acervo_receitas", "root", "Lukinhas34.");
+    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conexao->exec("set names utf8");
+} catch (PDOException $erro) {
+    echo "Erro na conexão:".$erro->getMessage();
+}
+ 
+// Bloco If que Salva os dados no Banco - atua como Create e Update
+if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "save" && $descricao != "") {
+    try {
+        if ($id != "") {
+            $stmt = $conexao->prepare("UPDATE g1_ingrediente SET nome=?,  descricao=? WHERE id = ?");
+            $stmt->bindParam(1, $nome);
+            $stmt->bindParam(2, $descricao);
+            $stmt->bindParam(3, $id);
+        } else {
+            $stmt = $conexao->prepare("INSERT INTO g1_ingrediente (nome, descricao) VALUES (?, ?)");
+        }
+        $stmt->bindParam(1, $nome);
+        $stmt->bindParam(2, $descricao);
+
+        if ($stmt->execute()) {
+            if ($stmt->rowCount() > 0) {
+                echo "Dados cadastrados com sucesso!";
+                $id = null;
+                $nome = null;
+                $descricao = null;
+
+            } else {
+                echo "Erro ao tentar efetivar cadastro";
+            }
+        } else {
+            throw new PDOException("Erro: Não foi possível executar a declaração sql");
+        }
+    } catch (PDOException $erro) {
+        echo "Erro: ".$erro->getMessage();
+    }
+}
+ 
+// Bloco if que recupera as informações no formulário, etapa utilizada pelo Update
+if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "upd" && $id != "") {
+    try {
+        $stmt = $conexao->prepare("SELECT * FROM g1_ingrediente WHERE id = ?");
+        $stmt->bindParam(1, $id, PDO::PARAM_INT);
+        if ($stmt->execute()) {
+            $rs = $stmt->fetch(PDO::FETCH_OBJ);
+            $id = $rs->id;
+            $nome = $rs-> nome;
+            $descricao = $rs->descricao;
+
+        } else {
+            throw new PDOException("Erro: Não foi possível executar a declaração sql");
+        }
+    } catch (PDOException $erro) {
+        echo "Erro: ".$erro->getMessage();
+    }
+}
+ 
+// Bloco if utilizado pela etapa Delete
+if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "del" && $id != "") {
+    try {
+        $stmt = $conexao->prepare("DELETE FROM g1_ingrediente WHERE id = ?");
+        $stmt->bindParam(1, $id, PDO::PARAM_INT);
+        if ($stmt->execute()) {
+            echo "Registo foi excluído com êxito";
+            $id = null;
+        } else {
+            throw new PDOException("Erro: Não foi possível executar a declaração sql");
+        }
+    } catch (PDOException $erro) {
+        echo "Erro: ".$erro->getMessage();
+    }
+}
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Ingredientes</title>
+    </head>
+    <body>
+        <form action="?act=save" method="POST" name="form1" >
+            <h1>Categorias</h1>
+            <hr>
+            <input type="hidden" name="id" <?php
+            // Preenche o id no campo id com um valor "value"
+            if (isset($id) && $id != null || $id != "") {
+                echo "value=\"{$id}\"";
+            }
+            ?> />
+            Ingrediente:
+            <input type="text" name="nome" <?php
+            // Preenche o nome no campo nome com um valor "value"
+            if (isset($nome) && $nome != null || $nome != ""){
+                echo "value=\"{$nome}\"";
+            }
+            ?> />   
+            Descricao:
+            <input type="text" name="descricao" <?php
+            // Preenche o nome no campo nome com um valor "value"
+            if (isset($descricao) && $descricao != null || $descricao != ""){
+                echo "value=\"{$descricao}\"";
+            }
+            ?> />
+
+           <input type="submit" value="salvar" />
+           <input type="reset" value="Novo" />
+           <hr>
+        </form>
+        <table border="1" width="100%">
+                <tr>
+                    <th>nome</th>
+                    <th>descrição</th>
+                    <th>Ações</th>
+                </tr>
+                <?php
+ 
+                // Bloco que realiza o papel do Read - recupera os dados e apresenta na tela
+                try {
+                    $stmt = $conexao->prepare("SELECT * FROM g1_ingrediente");
+                    if ($stmt->execute()) {
+                        while ($rs = $stmt->fetch(PDO::FETCH_OBJ)) {
+                            echo "<tr>";
+                            echo "<td>".$rs->nome."</td><td>".$rs->descricao
+                                       ."</td><td><center><a href=\"?act=upd&id=".$rs->id."\">[Alterar]</a>"
+                                       ."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+                                       ."<a href=\"?act=del&id=".$rs->id."\">[Excluir]</a></center></td>";
+                            echo "</tr>";
+                        }
+                    } else {
+                        echo "Erro: Não foi possível recuperar os dados do banco de dados";
+                    }
+                } catch (PDOException $erro) {
+                    echo "Erro: ".$erro->getMessage();
+                }
+                ?>
+            </table>
+            <a href="index.html"><h3>voltar ao menu anterior</h3></a>
+    </body>
+</html>
